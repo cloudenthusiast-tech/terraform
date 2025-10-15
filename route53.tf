cb9da901e06e9eb572167e1ecff958a7b6cf2f17
Aravind <kolanuaravindreddy@gmail.com>
2025-10-15 12:57:20 +0530

    terraform

--- route53.tf
+++ route53.tf
@@ -4,5 +4,5 @@ resource "aws_route53_record" "roboshop" {
   name    = "${var.instances[count.index]}" == "frontend" ? "${var.domain_name}" : "${var.instances[count.index]}.${var.domain_name}"  #mongodb.kolanu.space
   type    = "A"
   ttl     = 1
-  records = "${var.instances[count.index]}" == "frontend" ? [aws_instance.roboshop_ec2[count.index].public_ip] : [aws_instance.roboshop_ec2[count.index].private_ip] 
+  records = "${var.instances[count.index]}" == "frontend" ? [aws_instance.roboshop_ec2[count.index].public_ip] : [aws_instance.roboshop_ec2[count.index].private_ip]
 }
